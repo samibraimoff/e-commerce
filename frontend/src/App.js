76af750e77518b095dc8e7cfb6a24b5ad7a1893d
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-08-28 23:01:31 -0400

    add header, footer, install bootstrap, prettier, react-icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-const App = () => {
-    return (
-        <div>
-            <h1>welcome to e commerce</h1>
-        </div>
-    )
-}
+import { Container } from "react-bootstrap";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Fragment } from "react";
 
-export default App
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to E-Comm website</h1>
+        </Container>
+      </main>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default App;
